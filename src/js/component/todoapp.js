c98f81505447115dfce7635b3b5fca72f48831f6
Joshua <marichaljoshua@gmail.com>
2021-03-18 00:49:28 +0000

    v.2.1

--- src/js/component/todoapp.js
+++ src/js/component/todoapp.js
@@ -2,33 +2,40 @@ import React, { useState } from "react";
 
 export const TodoApp = () => {
 	let [lista, setLista] = useState([]);
+
 	const addTarea = tarea => {
 		if (tarea.key === "Enter") {
-			console.log(tarea);
 			setLista([...lista, tarea.target.value]);
-			console.log(lista);
 		}
 	};
 
-	const listItems = () => {
-		<li className="list-group-item">
-			Hola
-			<button className="btn btn-default btn-xs pull-right remove-item">
-				<i className="fas fa-trash-alt text-danger mx-2"></i>
-			</button>
-		</li>;
-	};
+	const items = [];
+
+	for (const [index, value] of lista.entries()) {
+		items.push(
+			<li className="list-group-item" key={index}>
+				{value}
+				<button className="btn btn-default btn-xs pull-right remove-item">
+					<i className="fas fa-trash-alt text-danger mx-2"></i>
+				</button>
+			</li>
+		);
+	}
 
 	return (
-		<div className="container">
-			<div className="row">
-				<div className="col-12 col-md-6">
-					<h1 className="text-muted">ToDos</h1>
-					<input
-						onKeyDown={addTarea}
-						className="form-control"
-						placeholder="What needs to be done?"></input>
-					<ul className="list-group">{listItems}</ul>
+		<div className="text-center mt-5">
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-md-6">
+						<h6 className="text-muted">To Do List</h6>
+						<input
+							onKeyDown={addTarea}
+							className="form-control"
+							type="text"
+							placeholder="Add a thing"></input>
+						<br />
+						<ul className="list-group">{items}</ul>
+					</div>
 				</div>
 			</div>
 		</div>

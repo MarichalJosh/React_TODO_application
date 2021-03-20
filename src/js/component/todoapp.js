b80e4e6195cb6bc9752b36d547800b0a071094ef
Joshua <marichaljoshua@gmail.com>
2021-03-20 16:25:03 +0000

    Final with Fetch

--- src/js/component/todoapp.js
+++ src/js/component/todoapp.js
@@ -1,11 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const TodoApp = () => {
 	let [lista, setLista] = useState([]);
 
+	useEffect(() => {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/marichaljoshua",
+			{
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		).then(resp => {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/marichaljoshua",
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}
+			)
+				.then(resp => {
+					console.log("respuesta", resp);
+					return resp.json();
+				})
+				.then(data => {
+					setLista(data);
+				})
+				.catch(error => {
+					//manejo de errores
+					console.log(error);
+				});
+		});
+	}, []);
+
 	const addTarea = tarea => {
 		if (tarea.key === "Enter") {
-			setLista([...lista, tarea.target.value]);
+			let newObj = {
+				label: tarea.target.value,
+				done: false
+			};
+			setLista([...lista, newObj]);
 			tarea.target.value = "";
 		}
 	};
@@ -13,12 +51,49 @@ export const TodoApp = () => {
 	function delTarea(pos) {
 		const tempList = [...lista];
 		tempList.splice(pos, 1);
-		setLista(tempList);
+		const methods = ["PUT", "DELETE"];
+		if (tempList.length > 0) {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/manulabarca",
+				{
+					method: methods[0],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					setLista(tempList);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		} else {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/manulabarca",
+				{
+					method: methods[1],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					console.log("Respuesta de borrado", resp);
+					setLista(tempList);
+					console.log(lista);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		}
 	}
 
 	const newList = lista.map((value, index) => (
 		<li className="list-group-item" key={index}>
-			{value}
+			{value.label}
 			<button
 				onClick={() => delTarea(index)}
 				className="btn btn-default remove-item float-end">
@@ -44,6 +119,9 @@ export const TodoApp = () => {
 				</div>
 			</div>
 			<p>Total Items: {lista.length}</p>
+			<button className="btn btn-info">
+				<i className="fa fa-trash"></i> Clear List
+			</button>
 		</div>
 	);
 };
